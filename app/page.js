dce161264606c9a76f613910a1d8018ec6b98473
kenderd <kenderd.dev@gmail.com>
2023-12-21 19:45:48 +0800

    nagdagdag ako ng yowyow

--- app/page.js
+++ app/page.js
@@ -8,15 +8,25 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-start justify-end p-24">
-     <h1 onClick={()=>{isHi(true)}} className='text-8xl'>
-      Hi
-     </h1>
-     { hi ?
-     <p onClick={()=> isRed(true)} className={`${red? "text-red-500 text-lg":"text-white text-lg"}`}>
-      Hello!
-     </p>
-     :""
-     }
+      <h1
+        onClick={() => {
+          isHi(true);
+        }}
+        className="text-8xl"
+      >
+        Hi
+      </h1>
+      {hi ? (
+        <p
+          onClick={() => isRed(true)}
+          className={`${red ? "text-red-500 text-lg" : "text-white text-lg"}`}
+        >
+          Hello!
+        </p>
+      ) : (
+        ""
+      )}
+      <h3>Yowyowyow</h3>
     </main>
-  )
+  );
 }
